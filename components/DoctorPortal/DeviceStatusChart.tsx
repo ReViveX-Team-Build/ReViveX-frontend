88cb85a83381e429c70ba63a7ce4405098e24c8e
dasuni <dasunisadeesha12@gmail.com>
2026-02-20 22:41:57 +0530

    add responsive PieChart with color-coded slices

--- components/DoctorPortal/DeviceStatusChart.tsx
+++ components/DoctorPortal/DeviceStatusChart.tsx
@@ -18,3 +18,21 @@ export default function DeviceStatusChart() {
     </div>
   );
 }
+
+<ResponsiveContainer width="100%" height={220}>
+  <PieChart>
+    <Pie
+      data={data}
+      cx="50%"
+      cy="50%"
+      innerRadius={60}
+      outerRadius={90}
+      dataKey="value"
+    >
+      {data.map((entry, index) => (
+        <Cell key={index} fill={COLORS[index]} />
+      ))}
+    </Pie>
+    <Tooltip />
+  </PieChart>
+</ResponsiveContainer>
