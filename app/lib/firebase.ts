cce41e058762013ec77a0e975df100284c085486
Alwis777 <isurialwis132@gmail.com>
2026-02-24 12:25:42 +0530

    Update code and protect firebase file from deletion

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,4 +1,5 @@
 // src/app/lib/firebase.js
+// Restoring this file - do not delete
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
