2c66c9922d0d6375bc3f5c7dd58f95622c03907e
Kisara10 <kisara.radev@gmail.com>
2026-03-02 08:36:45 +0530

    Completed the evaluation part

--- functions/index.js
+++ functions/index.js
@@ -10,4 +10,37 @@ exports.evaluateGameSession = functions.firestore
 
     const data = snap.data();
     const metrics = data.metrics;
+
+    if (!metrics) return null;
+
+    const strengthScore = Math.min((metrics.peakGripForce / 50) * 100, 100);
+
+    const enduranceScore = 100 - metrics.muscleEnduranceDropPercent;
+
+    let reactionScore = 100 - ((metrics.reactionTimeMs - 200) / 800) * 100;
+
+    reactionScore = Math.max(0, Math.min(reactionScore, 100));
+
+    const accuracyScore = metrics.cognitiveAccuracyPercent;
+
+    const overallScore = (0.30 * strengthScore) + (0.25 * enduranceScore) + 
+    (0.25 * reactionScore) + (0.25 * accuracyScore);
+
+    let status = overallScore >= 85 
+      ? "Optimal Recovery" 
+      : overallScore >= 70 
+      ? "Strong Progresss" 
+      : overallScore >= 50 
+      ? "Moderate" 
+      : "Need Attention";
+
+    return snap.ref.update({
+      strengthScore,
+      enduranceScore,
+      reactionScore,
+      accuracyScore,
+      overallScore,
+      status
+    });
+      
   })
